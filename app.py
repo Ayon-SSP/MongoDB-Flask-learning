42ba1e22ee000ecb88fd1569c922b0900d9bcabc
ayon_ssp <ayon.ssp@gmail.com>
2023-03-28 17:30:04 +0530

    Abele to Get the results in Postman

--- app.py
+++ app.py
@@ -1,76 +1,22 @@
 from flask import Flask, jsonify, request
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+app =  Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/users-db'
 
-# Set the MongoDB URI and database name
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
-app.config['MONGO_DBNAME'] = 'mydatabase'
+mongo = PyMongo(app)
 
-# Create a new PyMongo client and database instance
-client = MongoClient(app.config['MONGO_URI'])
-db = client[app.config['MONGO_DBNAME']]
+@app.route('/', methods=['GET'])
+def retrieveAll():
+    holder = []
+    currentCollection = mongo.db.users
+    for user in currentCollection.find():
+        holder.append({'id': user['id'], 'name': user['name'], 'email': user['email'], 'password': user['password']})
+        # holder.append({'1':'1'})
+        # print(user)
+    return jsonify(holder)
 
-# Define the User collection
-users = db.users
 
-# Define the REST API endpoints
 
-# GET /users - Returns a list of all users
-@app.route('/users', methods=['GET'])
-def get_users():
-    output = []
-    for user in users.find():
-        output.append({
-            'id': str(user['_id']),
-            'name': user['name'],
-            'email': user['email'],
-            'password': user['password']
-        })
-    return jsonify({'result': output})
-
-# GET /users/<id> - Returns the user with the specified ID
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-    user = users.find_one({'_id': ObjectId(id)})
-    if user:
-        output = {
-            'id': str(user['_id']),
-            'name': user['name'],
-            'email': user['email'],
-            'password': user['password']
-        }
-    else:
-        output = {'result': 'User not found'}
-    return jsonify(output)
-
-# POST /users - Creates a new user with the specified data
-@app.route('/users', methods=['POST'])
-def add_user():
-    data = request.get_json()
-    user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    user_id = users.insert_one(user).inserted_id
-    output = {'id': str(user_id)}
-    return jsonify(output)
-
-# PUT /users/<id> - Updates the user with the specified ID with the new data
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    data = request.get_json()
-    user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    result = users.update_one({'_id': ObjectId(id)}, {'$set': user})
-    if result.modified_count == 1:
-        output = {'result': 'User updated successfully'}
-    else:
-        output = {'result': 'User not found'}
-    return jsonify(output)
-
-# DELETE /users
+if __name__ == '__main__':
+    app.run(debug=True)
